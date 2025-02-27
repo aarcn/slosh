022965ee440c11e166f482a0487353c96692635b
aarcn <119821072+aarcn@users.noreply.github.com>
2025-02-27 06:28:43 -0600

    updated rtp math

--- sidebet/poker.py
+++ sidebet/poker.py
@@ -5,7 +5,8 @@ from cards import ranks, suits
 def poker_sidebet(master_count_map):
     """
     Returns RTP for the poker (21+3) side bet
-    Prints the calculated probabilities and RTP.
+    Example return value: 99.1234
+    Will print Calculated RTP vs Theoretical RTP (83.1803%)
 
     Payouts:
     Suited Three of a Kind: 100:1
@@ -19,11 +20,11 @@ def poker_sidebet(master_count_map):
     """
 
     total_cards = sum(master_count_map.values())
-    total_triples = math.comb(total_cards, 3)
+    total_three_card_combos = math.comb(total_cards, 3)
     straight_sequences = [ranks[i:i + 3] for i in range(len(ranks) - 2)] + [["A", "2", "3"]]
 
-    suited_three_kind_probability = 0
-    three_kind_probability = 0
+    suited_three_of_a_kind_probability = 0
+    three_of_a_kind_probability = 0
     straight_flush_probability = 0
     straight_probability = 0
     flush_probability = 0
@@ -33,7 +34,7 @@ def poker_sidebet(master_count_map):
         for suit in suits:
             suit_count = master_count_map.get(rank + suit, 0)
             if suit_count >= 3:
-                suited_three_kind_probability += math.comb(suit_count, 3) / total_triples
+                suited_three_of_a_kind_probability += math.comb(suit_count, 3) / total_three_card_combos
 
     # probability of drawing a three of a kind
     for rank in ranks:
@@ -41,8 +42,8 @@ def poker_sidebet(master_count_map):
         for suit in suits:
             count += master_count_map.get(rank + suit, 0)
         if count >= 3:
-            three_kind_probability += math.comb(count, 3) / total_triples
-    three_kind_probability -= suited_three_kind_probability
+            three_of_a_kind_probability += math.comb(count, 3) / total_three_card_combos
+    three_of_a_kind_probability -= suited_three_of_a_kind_probability
 
     # probability of drawing a straight flush
     for suit in suits:
@@ -51,7 +52,7 @@ def poker_sidebet(master_count_map):
             count2 = master_count_map.get(sequence[1] + suit, 0)
             count3 = master_count_map.get(sequence[2] + suit, 0)
 
-            if count1 > 0 and count2 > 0 and count3 > 0:  # Ensure all three cards exist
+            if count1 > 0 and count2 > 0 and count3 > 0:
                 straight_flush_probability += (
                         (math.comb(count1, 1) * math.comb(count2, 1) *
                          math.comb(count3, 1)) / math.comb(total_cards, 3))
@@ -73,17 +74,17 @@ def poker_sidebet(master_count_map):
             suit_count += master_count_map.get(rank + suit, 0)
         if suit_count >= 3:
             flush_combination = math.comb(suit_count, 3)
-            flush_probability += flush_combination / total_triples
-    flush_probability -= straight_flush_probability + suited_three_kind_probability
+            flush_probability += flush_combination / total_three_card_combos
+    flush_probability -= straight_flush_probability + suited_three_of_a_kind_probability
 
     # no_hand_probability is compliment of probability of drawing any hand
     no_hand_probability = 1 - (flush_probability + straight_probability +
-                               three_kind_probability + straight_flush_probability +
-                               suited_three_kind_probability)
+                               three_of_a_kind_probability + straight_flush_probability +
+                               suited_three_of_a_kind_probability)
 
     calculated_rtp = 100 + (((flush_probability * 4) + (straight_probability * 10) +
-                             (three_kind_probability * 20) + (straight_flush_probability * 30) +
-                             (suited_three_kind_probability * 100) + (no_hand_probability * -1)) * 100)
+                             (three_of_a_kind_probability * 20) + (straight_flush_probability * 30) +
+                             (suited_three_of_a_kind_probability * 100) + (no_hand_probability * -1)) * 100)
 
     theoretical_rtp = 83.1803
 
