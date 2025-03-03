273a49aa94e6008d87e1f7477225276bac0081dc
aarcn <119821072+aarcn@users.noreply.github.com>
2025-03-03 05:51:13 -0600

    testcases

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cards import *
 from sidebet.pairs import pairs_sidebet
 from sidebet.poker import poker_sidebet
+from sidebet.luckyladies import luckyladies_sidebet
 
 pairs_sidebet(master_count)
 poker_sidebet(master_count)
+luckyladies_sidebet(master_count)
